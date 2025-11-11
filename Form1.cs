bac62144e16eb97aca57ece1ba7802bd4e2debb7
MSI\renya <renya181206@gmail.com>
2025-11-11 11:52:25 +0900

    課題挿れるね

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 6; i++)
+                for (int j = 0; j < 4; j++)
+                {
+                    //インスタンスの生成
+                    testbotton testbotton = new testbotton();
+
+                    //ボタンの位置を設定 
+                    testbotton.Location = new Point(400 * i, 400);
+
+                    // ボタンの大きさを設定
+                    testbotton.Size = new Size(400, 400);
+
+                    // ボタン内のテキスト追加
+                    testbotton.Text = "あいうえお";
+
+                    //コントロールにボタンを設定
+                    Controls.Add(testbotton);
+                }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("感謝を忘れない");
+        }
+    }
+}
---
+++ testbotton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_test
+{
+
+    internal class testbotton : Button
+    {
+
+    }
+}
